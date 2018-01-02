3dc55a6283ed937633bd591ebccc2326451605b9
Michael Hadley <mikethadley@gmail.com>
2018-01-01 20:08:51 -0800

    Redundant docs

--- src/Noodle.elm
+++ src/Noodle.elm
@@ -7,7 +7,7 @@ module Noodle
         , update
         )
 
-{-| A *starchy* library for managing application state.
+{-| Basic usage in your standard *counter* app:
 
     import Noodle
 
@@ -42,14 +42,6 @@ module Noodle
         case msg of
             Click ->
                 ( model, increment )
-
-If you like to organize your models in to seperate modules, and then embed those models
-into other modules, you may find that sometimes there is state that you want to persist at
-a higher level. You can use this library to help manage that state, and `Action`s that updaate
-that state.
-
-**Noodle** has taken a lot of inspiration from [`elm-taco`](https://github.com/ohanhi/elm-taco) and
-[`redux`](https://redux.js.org/).
 
 
 # Creating an `Action`
